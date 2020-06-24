4f2910a2108d779003e351e363f095179ae2d67b
George-Pereira <fernanr8@gmail.com>
2020-06-24 18:59:54 -0300

    Base feita

--- src/com/fatec/farmacia/controller/ServletLogin.java
+++ src/com/fatec/farmacia/controller/ServletLogin.java
@@ -8,7 +8,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@WebServlet("/login")
+@WebServlet("/logon")
 public class ServletLogin extends HttpServlet 
 {
 	@Override
